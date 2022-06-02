d29b33bcb8d31ff30603d8d92fa943ef7f0db43a
OptimizedFunction <95468660+OptimizedFunction@users.noreply.github.com>
2022-06-02 17:00:51 +0530

    Initial update

--- src/ServerScriptService/Script.server.lua
+++ src/ServerScriptService/Script.server.lua
@@ -1,35 +1,32 @@
---local statusTracker = require(script.Parent.Matchmaker.PlayerMatchmakingStatusTracker)
---local matchmaker = require(script.Parent.Matchmaker)
---local RS = game:GetService("ReplicatedStorage")
---local pushRemote = RS.PushRemote
+local statusTracker = require(script.Parent.Matchmaker.PlayerMatchmakingStatusTracker)
+local matchmaker = require(script.Parent.Matchmaker)
+local RS = game:GetService("ReplicatedStorage")
+local pushRemote = RS.PushRemote
 
---matchmaker.InitialiseMatchmaker({""})
+matchmaker.InitialiseMatchmaker({""})
 
---pushRemote.OnServerInvoke = function(plr : Player, gamemode : string)
---	if not statusTracker[plr.UserId] then
---		local queued = matchmaker.AddPlayerToQueue(plr, gamemode) 
---		return queued
---	else
---		local queued = matchmaker.RemovePlayerFromQueue(plr, gamemode)
---		return not queued
---	end
---end
+pushRemote.OnServerInvoke = function(plr : Player, gamemode : string)
+	if not statusTracker[plr.UserId] then
+		local queued = matchmaker.AddPlayerToQueue(plr, gamemode) 
+		return queued
+	else
+		local queued = matchmaker.RemovePlayerFromQueue(plr, gamemode)
+		return not queued
+	end
+end
 
---game:GetService("Players").PlayerAdded:Connect(function(plr : Player)
---	wait(10)
---	local isReserved = game.PrivateServerId ~= "" and game.PrivateServerOwnerId == 0
+game:GetService("Players").PlayerAdded:Connect(function(plr : Player)
+	wait(10)
+	local isReserved = game.PrivateServerId ~= "" and game.PrivateServerOwnerId == 0
 	
---	if isReserved then return end
+	if isReserved then return end
 
---	local TeleportService = game:GetService("TeleportService")
---	local serverAccessCode = TeleportService:ReserveServer(game.PlaceId)
---	TeleportService:TeleportToPrivateServer(game.PlaceId, serverAccessCode, {plr}, nil, "hi")
---end)
+	local TeleportService = game:GetService("TeleportService")
+	local serverAccessCode = TeleportService:ReserveServer(game.PlaceId)
+	TeleportService:TeleportToPrivateServer(game.PlaceId, serverAccessCode, {plr}, nil, "hi")
+end)
 
---while true do
---	task.wait(5)
---	matchmaker.MatchPlayers("1", 2, 10)
---end
-
-local module = require(game.ServerScriptService.ModuleScript)
-print(module.getMyTable())
+while true do
+	task.wait(5)
+	matchmaker.MatchPlayers("1", 2, 10)
+end
